19b22b4e4344b7aa3cc6994bd39b2af733e3cb7c
DongGyu Lee <dlehdrb1129@gmail.com>
2020-08-24 18:38:16 +0900

    20200824 login DB update

--- first_project/urls.py
+++ first_project/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path("main/", views.main, name="main"),
     path('register/', views.register, name="register"),
+    path('newLogin/', views.newLogin, name="newLogin"),
+    path('sign_in/', views.sign_in, name="sign_in"),
 ]
--- first_project/views.py
+++ first_project/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from django.template import loader
 from django.http import HttpResponse
-from .models import Upload
+from django.contrib.auth.hashers import make_password, check_password
+from .models import Upload, Users
 import datetime
 from django.contrib import messages
 
@@ -27,3 +28,48 @@ def register(request):
     context.save()
     messages.success(request, "신고 접수 완료되었습니다!")
     return redirect("main")
+
+def newLogin(request):
+    context = None
+    if request.method == "POST":
+        useremail = request.POST.get('useremail', None)
+        password = request.POST.get('password', None)
+        try :
+            user = Users.objects.get(useremail=useremail)
+        except Users.DoesNotExist :
+            context = {'error': '아이디를 확인하세요'}
+        else :
+            if check_password(password, user.password):
+                request.session['user'] = useremail
+                return redirect('onlymember')
+            else :
+                context = { 'error' : '패스워드를 확인하세요'}
+    else :
+        if 'user' in request.session:
+            context = {'msg': '이미 로그인 하셨습니다.'}
+    return render(request, 'main.html', context)  ## 로그인 후 홈페이지로 고쳐야 함
+
+def sign_in(request):
+    if request.method =='GET':
+        return render(request, 'main.html')
+    elif request.method == 'POST':
+        useremail = request.POST.get('useremail', None)
+        username = request.POST.get('username', None)
+        password = request.POST.get('password', None)
+        re_password = request.POST.get('re-password',None)
+        res_data = {}
+        if not (username and password and re_password and useremail):
+            res_data['error']='아이디 또는 패스워드를 입력해주세요.'
+        elif password != re_password:
+            res_data['error']='비밀번호가 다릅니다.'
+        else:
+            users = Users(
+                useremail=useremail,
+                username=username,
+                password=make_password(password),
+            )
+            users.save()
+            # return redirect('main')
+
+        # return render(request, res_data)
+        return render(request, 'main.html', res_data)
