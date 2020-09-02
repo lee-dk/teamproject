c43f7e37fcf1a35666a42179321e7c8333715ac8
DongGyuLee <dlehdrb1129@gmail.com>
2020-09-02 11:57:27 +0900

    20200901 teamproject detail update

--- first_project/models.py
+++ first_project/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
 # Create your models here.
 class Users(models.Model):
@@ -18,6 +19,9 @@ class Upload(models.Model):
     datetime = models.DateTimeField(null=True)
     gender = models.TextField()
     feature = models.TextField()
-    phone = models.CharField(max_length=15)
+    # phone = models.CharField(max_length=15)
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$',
+                                 message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+    phone = models.CharField(validators=[phone_regex], max_length=17, blank=True)
     place = models.TextField()
     photo = models.FileField(blank=True)  #, upload_to="photo_%Y_%m_%d")
